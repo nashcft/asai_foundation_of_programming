752da0bbd82e3efa08f8574b1c3b586f8d0c65f6
nashcft <nashrfv@gmail.com>
2018-01-05 00:36:42 +0900

    Exercise 7.2

--- chapter_07/exercises.ml
+++ chapter_07/exercises.ml
@@ -8,3 +8,11 @@ let goukei_to_heikin l m e ns ss = (total5 l m e ns ss, (total5 l m e ns ss) /. 
 (* example *)
 let result1 = goukei_to_heikin 70. 75. 80. 85. 85.
 let result2 = goukei_to_heikin 27. 83. 54. 97. 87.
+
+(* 7.2. 名前と成績の組から "<name>さんの評価は<grade>です"という文字列を生成する *)
+(* seiseki : string * string -> string *)
+let seiseki p = match p with (name, grade) -> name ^ "さんの評価は" ^ grade ^ "です"
+
+(* tests *)
+let test_seiseki1 = seiseki ("鈴木", "A") = "鈴木さんの評価はAです"
+let test_seiseki2 = seiseki ("佐藤", "B") = "佐藤さんの評価はBです"
