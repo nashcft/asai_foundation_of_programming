fd5e1cf6bff24cd5d8a2eb01e4dadcf8f90458a1
nashcft <nashrfv@gmail.com>
2018-04-01 16:27:18 +0900

    13.3-13.5

--- chapter_13/exercises.ml
+++ chapter_13/exercises.ml
@@ -22,3 +22,19 @@ let test_13_1_2 = person_namae [
     { name = "Carol"; height = 160.; weight = 60.; birth_month = 9; birth_day = 14; blood_type = "AB" };
     { name = "Dave"; height = 170.; weight = 70.; birth_month = 5; birth_day = 21; blood_type = "A" };
 ] = [ "Alice"; "Bob"; "Carol"; "Dave"]
+
+(* 13.3 *)
+let f1 a = a
+let f2 a b = a
+let f3 a b = b
+let f4 a f = f a
+let f5 f g a = g (f a)
+
+(* 13.4 *)
+let compose f g = let h x = f (g x) in h
+(* compose : ('a -> 'b) -> ('c -> 'a) -> 'c -> 'a *)
+
+(* 13.5:
+ * (twice twice) : ('_weak1 -> '_weak1) -> '_weak1 -> '_weak1
+ * (twice twice) is a function that applies the given function 2 ^ 2 times to its argument
+ *)
