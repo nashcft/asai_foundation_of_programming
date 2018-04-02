a586a52c220c894d25b498ae93d232a65bd09f6e
nashcft <nashrfv@gmail.com>
2018-04-03 08:01:04 +0900

    14.8 - 14.10

--- chapter_14/exercises.ml
+++ chapter_14/exercises.ml
@@ -1,7 +1,5 @@
 (* 14.1 *)
-let even l = 
-    let is_even n = n mod 2 = 0 in
-        List.filter is_even l
+let even l = List.filter (fun n -> n mod 2 = 0) l
 
 (* 14.1 tests (from 9.5) *)
 let test_9_5_1 = even [] = []
@@ -16,9 +14,8 @@ type gakusei_t = {
     grade: string;
 }
 
-let count_A l = 
-    let grade_A s = match s with { name = n; score = s; grade =g; } -> g = "A" in 
-        List.length (List.filter grade_A l)
+let count_A l =
+    List.length (List.filter (fun s -> match s with { name = n; score = s; grade =g; } -> g = "A") l)
 
 (* 14.2 tests (from textbook) *)
 let slist = [
@@ -33,9 +30,7 @@ let test_14_2_3 = count_A [{ name = ""; score = 1; grade = "B" }; { name = ""; s
 let test_14_2_4 = count_A slist = 2
 
 (* 14.3 *)
-let concat (l: string list) : string = 
-    let concat_strings s1 s2 = s1 ^ s2 in 
-        List.fold_right concat_strings l ""
+let concat (l: string list) : string = List.fold_right (fun s1 s2 -> s1 ^ s2) l ""
 
 (* 14.3 tests *)
 let test_14_3_1 = concat [] = ""
@@ -43,9 +38,8 @@ let test_14_3_2 = concat ["a"; "b"; "c"] = "abc"
 
 (* 14.4 *)
 let gakusei_sum (l: gakusei_t list) : int = 
-    let sum_score (s: gakusei_t) (n: int) : int = match s with
-        { name = nm; score = sc; grade = gr } -> sc + n in
-            List.fold_right sum_score l 0
+    List.fold_right (fun (s: gakusei_t) (n: int) -> match s with
+        { name = nm; score = sc; grade = gr } -> sc + n) l 0
 
 (* 14.4 tests *)
 let test_14_4_1 = gakusei_sum [] = 0
@@ -57,10 +51,30 @@ let test_14_4_2 = gakusei_sum [
 
 (* 14.6 *)
 let count (l: gakusei_t list) (target: string) : int =
-    let match_grade s = match s with 
-        { name = nm; score = sc; grade = gr } -> gr = target in
-            List.length (List.filter match_grade l)
+    List.length (List.filter (fun s -> match s with
+        { name = nm; score = sc; grade = gr } -> gr = target) l)
 
+(* 14.6 tests *)
 let test_14_6_1 = count slist "A" = 2
 let test_14_6_2 = count slist "B" = 1
 let test_14_6_3 = count slist "C" = 0
+
+(* 14.8 *)
+let f14_8 =fun n -> n * n - 1
+
+(* 14.9 *)
+type person_t = {
+    name : string;
+    height : float;
+    weight : float;
+    birth_month : int;
+    birth_day : int;
+    blood_type : string;
+}
+let f14_9 = fun p -> match p with 
+    { name = n;
+    height = h;
+    weight = w;
+    birth_month = m;
+    birth_day = d;
+    blood_type = b } -> n
