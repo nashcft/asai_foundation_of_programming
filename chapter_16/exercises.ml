ee4934e6f7ace1788b9b6645d971dc86c07b76de
nashcft <nashrfv@gmail.com>
2018-04-06 10:05:27 +0900

    16.2

--- chapter_16/exercises.ml
+++ chapter_16/exercises.ml
@@ -6,3 +6,12 @@ let sum_list l =
     inner l 0
 
 let test_16_1 = sum_list [3; 2; 1; 4] = [3; 5; 6; 10]
+
+(* 16.2 *)
+let rec fold_left f init l = match l with
+    [] -> init
+    | x::xs -> fold_left f (f init x) xs
+
+let test_16_2_1 = fold_left (+) 0 [3; 2; 1; 4] = 10
+let test_16_2_2 = fold_left (+) 0 [3; 2; 1; 4] = List.fold_left (+) 0 [3; 2; 1; 4]
+let test_16_2_2 = fold_left (+) 0 [3; 2; 1; 4] = List.fold_right (+) [3; 2; 1; 4] 0
