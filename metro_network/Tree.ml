6671a8dfdb9e7c0852efd1a069bed922209ff749
nashcft <nashrfv@gmail.com>
2018-04-11 09:02:52 +0900

    Signature

--- metro_network/Tree.ml
+++ metro_network/Tree.ml
@@ -1,19 +1,29 @@
 module Tree = struct
-    type ('a, 'b) t = Empty
-                        | Node of 'a * 'b * ('a, 'b) t * ('a, 'b) t
+    type ('a, 'b) t = 
+        Empty
+        | Node of 'a * 'b * ('a, 'b) t * ('a, 'b) t
     let empty = Empty
 
-    let rec insert (tree: ('a, 'b) t) (k: 'a) (v: 'b) : ('a, 'b) t = match tree with
+    let rec insert tree k v = match tree with
         Empty -> Node (k, v, Empty, Empty)
         | Node (key, value, l, r) ->
             if k = key then Node (key, v, l, r)
             else if k < key then Node (key, value, insert l k v, r)
             else Node (key, value, l, insert r k v)
     
-    let rec search (tree: ('a, 'b) t) (k: 'a) : 'b = match tree with
+    let rec search tree k = match tree with
         Empty -> raise Not_found
         | Node (key, value, l, r) ->
             if k = key then value
             else if k < key then search l k
             else search r k
+end
+
+module type Tree_t = sig
+    type ('a, 'b) t
+    val empty: ('a, 'b) t
+
+    val insert : ('a, 'b) t -> 'a -> 'b -> ('a, 'b) t
+
+    val search : ('a, 'b) t -> 'a -> 'b
 end
